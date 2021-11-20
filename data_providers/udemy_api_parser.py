399d64615c528d20c43ca64fbdfdcd3ebcd81e44
Vasileios Vyzas <vizasb@gmail.com>
2021-11-20 17:42:35 +0000

    Corrected the way of updating the json and cleaned up a bit the code

--- data_providers/udemy_api_parser.py
+++ data_providers/udemy_api_parser.py
@@ -5,10 +5,6 @@ import json
 from requests.auth import HTTPBasicAuth
 from datetime import datetime
 
-client_id = 'nzCKl5gJKWcSoXXnNU5RB6XXNyeColzpwEAfapaA'
-client_secret = 'dROYzyuO7IpEi8nE9fyUbrECowtuCfxHodoCQ53PgB7dg2JFblpSrVz4tsjjfmCYNbOtAIT9bEnYKderbVSAyK7ucLOIRfaxwmHtZ0pkfl90zqtUUikHiOn6DJ1gfpso'
-
-
 def request_courses_list(url):
     courses_list = requests.get(url, auth=HTTPBasicAuth(client_id, client_secret))
     return courses_list.json()
@@ -23,12 +19,16 @@ def get_next_page(courses_list):
 
 def write_json(new_data, filename):
     with open(filename, 'r+') as file:
+
         # First we load existing data into a dict.
         file_data = json.load(file)
+
         # Join new_data with file_data inside emp_details
-        file_data["results"].append(new_data)
+        file_data['results'].extend(new_data)
+
         # Sets file's current position at offset.
         file.seek(0)
+
         # convert back to json.
         json.dump(file_data, file, indent=4)
 
@@ -36,13 +36,14 @@ def write_json(new_data, filename):
 def get_data_from_new_page_and_write_to_file(page, filename):
     if page is not None:
         courses_list = request_courses_list(page)
-        for result in courses_list['results']:
-            print(result)
-            write_json(result, filename)
+        courses = courses_list['results']
+
+        write_json(courses, filename)
+
         page = get_next_page(courses_list)
-        get_data_from_new_page_and_write_to_file(page,
-                                                 os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)),
-                                                              'data', filename))
+        get_data_from_new_page_and_write_to_file(page, filename)
+    else:
+        print('Reached the end of pages')
 
 
 def main():
@@ -50,16 +51,14 @@ def main():
     courses_list = request_courses_list(root_endpoint)
 
     file_name = str(datetime.now().date()) + '.json'
-    with open(os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), 'data', file_name),
-              'w') as outputfile:
-        json.dump(courses_list, outputfile)
+    file_location = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), 'data', file_name)
+
+    with open(file_location, 'w') as output_file:
+        json.dump(courses_list, output_file)
 
     page = get_next_page(courses_list)
     print(page)
-    if page is not None:
-        get_data_from_new_page_and_write_to_file(page,
-                                                 os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)),
-                                                              'data', file_name))
+    get_data_from_new_page_and_write_to_file(page, file_location)
 
 
 if __name__ == "__main__":
