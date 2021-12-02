abf7399eed521c6408e82483e8c5c7f9dd993fa3
Vasileios Vyzas <vizasb@gmail.com>
2021-12-02 07:29:40 +0000

    Added functions to scrape the course info from each topic

--- data_providers/MIT_scapper.py
+++ data_providers/MIT_scapper.py
@@ -3,13 +3,34 @@ import json
 import os
 from bs4 import BeautifulSoup
 
+def get_course_info(course_url):
+    course = requests.get(f"https://ocw.mit.edu/{course_url}")
+
+    soup1 = BeautifulSoup(course.text, 'lxml')
+
+    description = soup1.find_all('div', {'id': 'description'})
+
+
+
+
+def topic_related_courses(topic):
+    course_list = requests.get(f"https://ocw.mit.edu/courses/find-by-topic/" + topic).json()
+
+    for course_info in course_list:
+        print(course_info['id'], course_info['title'], course_info['href'])
+        get_course_info(course_info['href'])
+        break
+
 
 def main():
     topics_url = 'https://ocw.mit.edu/courses/find-by-topic/topics.json'
 
     topics = requests.get(topics_url).json()
     for topic in topics:
-        print(topic)
+        print(topic['name'])
+        print(topic['file'])
+        topic_related_courses(topic['file'])
+        break
 
 
 if __name__ == '__main__':
