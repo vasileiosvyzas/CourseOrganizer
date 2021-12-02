301c63a61a10f5ccc0be08b7b4ed2de274d2638e
Vasileios Vyzas <vizasb@gmail.com>
2021-12-02 19:10:06 +0000

    Finished the function to get the course description from each page

--- data_providers/MIT_scapper.py
+++ data_providers/MIT_scapper.py
@@ -2,24 +2,32 @@ import requests
 import json
 import os
 from bs4 import BeautifulSoup
+from lxml import etree
 
-def get_course_info(course_url):
+
+def get_course_description(course_url):
     course = requests.get(f"https://ocw.mit.edu/{course_url}")
 
-    soup1 = BeautifulSoup(course.text, 'lxml')
+    soup1 = BeautifulSoup(course.content, 'html.parser')
 
-    description = soup1.find_all('div', {'id': 'description'})
+    dom = etree.HTML(str(soup1))
+    description = dom.xpath('//*[@id="description"]/div/p/text()')
 
-
+    return "".join(description)
 
 
 def topic_related_courses(topic):
     course_list = requests.get(f"https://ocw.mit.edu/courses/find-by-topic/" + topic).json()
+    course_info = {}
 
-    for course_info in course_list:
-        print(course_info['id'], course_info['title'], course_info['href'])
-        get_course_info(course_info['href'])
-        break
+    for course_data in course_list:
+        course_info['course_id'] = course_data['id']
+        course_info['course_title'] = course_data['title']
+        course_info['course_link'] = f"https://ocw.mit.edu/{course_data['href']}"
+        course_info['description'] = get_course_description(course_data['href'])
+        course_info['semester'] = course_data['sem']
+        course_info['level'] = course_data['level']
+        course_info['department'] = course_data['department']
 
 
 def main():
