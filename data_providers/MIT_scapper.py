669ac98f975c77046fdd952b8a29f95a16d8e056
Vasileios Vyzas <vizasb@gmail.com>
2021-12-03 08:22:47 +0000

    Added two functions to write the data to a json file

--- data_providers/MIT_scapper.py
+++ data_providers/MIT_scapper.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from datetime import datetime
 from bs4 import BeautifulSoup
 from lxml import etree
 
@@ -19,7 +20,7 @@ def get_course_description(course_url):
 def topic_related_courses(topic):
     course_list = requests.get(f"https://ocw.mit.edu/courses/find-by-topic/" + topic).json()
     course_info = {}
-
+    course_info_list = []
     for course_data in course_list:
         course_info['course_id'] = course_data['id']
         course_info['course_title'] = course_data['title']
@@ -29,16 +30,42 @@ def topic_related_courses(topic):
         course_info['level'] = course_data['level']
         course_info['department'] = course_data['department']
 
+        course_info_list.append(course_info)
+
+    return course_info_list
+
+
+def write_json(new_data, filename):
+    with open(filename, 'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+
+        # Join new_data with file_data inside emp_details
+        file_data.extend(new_data)
+
+        # Sets file's current position at offset.
+        file.seek(0)
+
+        # convert back to json.
+        json.dump(file_data, file, indent=4)
+
 
 def main():
     topics_url = 'https://ocw.mit.edu/courses/find-by-topic/topics.json'
-
     topics = requests.get(topics_url).json()
+
+    file_name = str(datetime.now().date()) + '.json'
+    file_location = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), 'data', file_name)
+
+    with open(file_location, 'w') as output_file:
+        json.dump([], output_file)
+
     for topic in topics:
-        print(topic['name'])
-        print(topic['file'])
-        topic_related_courses(topic['file'])
-        break
+        print(f"Getting the course info for {topic['file']}")
+        courses_info = topic_related_courses(topic['file'])
+
+        print('Writing the info to a json file')
+        write_json(courses_info, file_location)
 
 
 if __name__ == '__main__':
