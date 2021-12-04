5b6d5b4e3d6e611ff30357619bf6d87de990bf8f
Vasileios Vyzas <vizasb@gmail.com>
2021-12-04 17:46:35 +0000

    Created dictionary with the course information

--- data_providers/MIT_new_courses_scrapper.py
+++ data_providers/MIT_new_courses_scrapper.py
@@ -6,34 +6,6 @@ from bs4 import BeautifulSoup
 from lxml import etree
 
 
-def get_course_description(course_url):
-    course = requests.get(f"https://ocw.mit.edu/{course_url}")
-
-    soup1 = BeautifulSoup(course.content, 'html.parser')
-
-    dom = etree.HTML(str(soup1))
-    description = dom.xpath('//*[@id="description"]/div/p/text()')
-
-    return "".join(description)
-
-
-def topic_related_courses(topic):
-    course_list = requests.get(f"https://ocw.mit.edu/courses/find-by-topic/" + topic).json()
-    course_info = {}
-    course_info_list = []
-    for course_data in course_list:
-        course_info['course_id'] = course_data['id']
-        course_info['course_title'] = course_data['title']
-        course_info['course_link'] = f"https://ocw.mit.edu/{course_data['href']}"
-        course_info['description'] = get_course_description(course_data['href'])
-        course_info['semester'] = course_data['sem']
-        course_info['level'] = course_data['level']
-        course_info['department'] = course_data['department']
-
-        course_info_list.append(course_info)
-
-    return course_info_list
-
 
 def write_json(new_data, filename):
     with open(filename, 'r+') as file:
@@ -50,12 +22,44 @@ def write_json(new_data, filename):
         json.dump(file_data, file, indent=4)
 
 
+def get_course_level(course_page):
+    """
+
+    :param course_page:
+    :return: course level as string
+    """
+    course = requests.get(course_page)
+
+    soup2 = BeautifulSoup(course.content, 'html.parser')
+
+    level_tag = soup2.find('h3', text='Level')
+
+    if level_tag is None:
+        return None
+    else:
+        return level_tag.find_next_sibling('p').text
+
+
 def main():
     topics_url = 'https://ocw.mit.edu/rss/new/mit-newcourses.xml'
     new_courses = requests.get(topics_url).content
-
+    course_info = {}
     soup1 = BeautifulSoup(new_courses, 'lxml')
+    new_courses = soup1.find_all("item")
 
+    print(new_courses)
+    course_list = []
+    for course in new_courses:
+        course_info['title'] = course.title.text
+        course_info['description'] = course.description.text
+        course_info['url'] = course['rdf:about']
+        course_info['semester'] = course.fromsemester.text
+        course_info['year'] = course.fromyear.text
+        course_info['level'] = get_course_level(course['rdf:about'])
+
+        course_list.append(course_info)
+
+    print(course_list)
 
 
 if __name__ == '__main__':
