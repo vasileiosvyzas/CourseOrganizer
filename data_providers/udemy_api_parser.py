921efd4751747e4394b6fa9ab3fde1c2bb338676
Vasileios Vyzas <vizasb@gmail.com>
2022-02-26 16:15:28 +0000

    Added exception handling to the s3 bucket upload function

--- data_providers/udemy_api_parser.py
+++ data_providers/udemy_api_parser.py
@@ -8,11 +8,11 @@ from requests.auth import HTTPBasicAuth
 from datetime import datetime
 
 
-def request_courses_list(url: str) -> Dict:
+def request_courses_list(url: str, session) -> Dict:
     try:
-        response = requests.get(url,
-                                auth=HTTPBasicAuth(os.environ.get('UDEMY_CLIENT_ID'),
-                                                   os.environ.get('UDEMY_SECRET')))
+        response = session.get(url,
+                               auth=HTTPBasicAuth(os.environ.get('UDEMY_CLIENT_ID'),
+                                                  os.environ.get('UDEMY_SECRET')))
 
         response.raise_for_status()
         return response.json()
@@ -26,42 +26,6 @@ def request_courses_list(url: str) -> Dict:
         print(err)
 
 
-# def get_next_page(courses_list):
-#     if courses_list['next'] is not None:
-#         return courses_list['next']
-#     else:
-#         return None
-
-
-# def write_json(new_data, filename):
-#     with open(filename, 'r+') as file:
-#         # First we load existing data into a dict.
-#         file_data = json.load(file)
-#
-#         # Join new_data with file_data inside emp_details
-#         file_data['results'].extend(new_data)
-#
-#         # Sets file's current position at offset.
-#         file.seek(0)
-#
-#         # convert back to json.
-#         json.dump(file_data, file, indent=4)
-
-
-# def get_data_from_new_page_and_write_to_file(page, filename):
-#     if page is not None:
-#         courses_list = request_courses_list(page)
-#         courses = courses_list['results']
-#
-#         print('Updating the json')
-#         write_json(courses, filename)
-#
-#         print('Getting the new page and start all over again')
-#         page = get_next_page(courses_list)
-#         get_data_from_new_page_and_write_to_file(page, filename)
-#     else:
-#         print('Reached the end of pages')
-
 def write_data_to_json_on_disk(courses_list: List) -> None:
     file_name = str(datetime.now().date()) + '.json'
     file_location = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), 'data', file_name)
@@ -70,18 +34,39 @@ def write_data_to_json_on_disk(courses_list: List) -> None:
         json.dump(courses_list, output_file)
 
 
+def upload_courses_list_file_to_s3_bucket(file_location: str) -> None:
+    """
+    It takes the filename of the json file with all the courses and it will upload it to
+    the S3 bucket
+    :param file_location:
+    :return:
+    """
+    try:
+        s3_resource = boto3.resource('s3')
+        date = datetime.now()
+        filename = f"udemy/{date.date()}.json"
+        response = s3_resource.Object(Bucket='', Key=filename).upload_file(f"/tmp/{file_location}")
+    except Exception as e:
+        print('something went wrong')
+
+
 def main():
+    session = requests.Session()
     root_endpoint = 'https://www.udemy.com/api-2.0/courses/'
-    courses_list = request_courses_list(root_endpoint)
+    courses_list = request_courses_list(root_endpoint, session)
 
     all_courses = courses_list['results']
-    result = 0
     while courses_list['next'] is not None:
         next_page = courses_list['next']
-        courses_list = request_courses_list(next_page)
+        print(next_page)
+        courses_list = request_courses_list(next_page, session)
         all_courses.extend(courses_list['results'])
 
-    write_data_to_json_on_disk(all_courses)
+    # write_data_to_json_on_disk(all_courses)
+
+    file_name = str(datetime.now().date()) + '.json'
+    file_location = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), 'data', file_name)
+    upload_courses_list_file_to_s3_bucket(file_location=file_location)
 
 
 if __name__ == "__main__":
