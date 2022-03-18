f4abd46327fac26da118909a32d7effecd33cb9e
Vasileios Vyzas <vizasb@gmail.com>
2022-03-18 06:54:40 +0000

    Added exception handling to the s3 bucket upload function

--- data_providers/udemy_api_parser.py
+++ data_providers/udemy_api_parser.py
@@ -26,27 +26,26 @@ def request_courses_list(url: str, session) -> Dict:
         print(err)
 
 
-def write_data_to_json_on_disk(courses_list: List) -> None:
-    file_name = str(datetime.now().date()) + '.json'
-    file_location = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), 'data', file_name)
-
+def write_data_to_json_on_disk(courses: List, file_location: str) -> None:
     with open(file_location, 'w') as output_file:
-        json.dump(courses_list, output_file)
+        json.dump(courses, output_file)
 
 
-def upload_courses_list_file_to_s3_bucket(file_location: str) -> None:
+def upload_courses_list_file_to_s3_bucket(file_location: str, filename: str) -> None:
     """
     It takes the filename of the json file with all the courses and it will upload it to
     the S3 bucket
+    :param filename:
     :param file_location:
     :return:
     """
     try:
-        s3_resource = boto3.resource('s3')
-        date = datetime.now()
-        filename = f"udemy/{date.date()}.json"
-        response = s3_resource.Object(Bucket='', Key=filename).upload_file(f"/tmp/{file_location}")
+        print('uploading to s3')
+        session = boto3.Session(profile_name='iamadmin-portofolio-general')
+        s3_resource = session.resource('s3')
+        s3_resource.Bucket('udemy-courses-store').upload_file(file_location, filename)
     except Exception as e:
+        print(e)
         print('something went wrong')
 
 
@@ -62,11 +61,13 @@ def main():
         courses_list = request_courses_list(next_page, session)
         all_courses.extend(courses_list['results'])
 
-    # write_data_to_json_on_disk(all_courses)
-
     file_name = str(datetime.now().date()) + '.json'
     file_location = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), 'data', file_name)
-    upload_courses_list_file_to_s3_bucket(file_location=file_location)
+
+    write_data_to_json_on_disk(courses=all_courses, file_location=file_location)
+
+    print(file_location)
+    upload_courses_list_file_to_s3_bucket(file_location=file_location, filename=file_name)
 
 
 if __name__ == "__main__":
